fd75ff348a907ce988d8e5e4cd86b4062a369ab5
raylison nunes dos santos <raylison100@gmail.com>
2019-09-20 08:04:39 -0300

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float velocidade;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movimentar();
+    }
+
+    void Movimentar()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            transform.Translate(Vector2.right * velocidade * Time.deltaTime);
+        }
+
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.Translate(-Vector2.right * velocidade * Time.deltaTime);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Tutorial-Plataforma-2D
+ 
