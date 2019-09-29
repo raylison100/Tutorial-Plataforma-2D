1621feab623d2b6e6c1f8a9a9161b0a441d42d1a
raylison nunes dos santos <raylison100@gmail.com>
2019-09-29 18:23:22 -0300

    update tutorial

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,10 +7,14 @@ public class Player : MonoBehaviour
 
     public float velocidade;
 
+    public Transform player;
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = player.GetComponent<Animator>();
+        animator.speed = 0.25f;
     }
 
     // Update is called once per frame
@@ -21,14 +25,19 @@ public class Player : MonoBehaviour
 
     void Movimentar()
     {
+
+        animator.SetFloat("run", Mathf.Abs(Input.GetAxis("Horizontal")));
+         
         if (Input.GetAxisRaw("Horizontal") > 0)
         {
             transform.Translate(Vector2.right * velocidade * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
         }
 
         if (Input.GetAxisRaw("Horizontal") < 0)
         {
-            transform.Translate(-Vector2.right * velocidade * Time.deltaTime);
+            transform.Translate(Vector2.right * velocidade * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 180);
         }
     }
 }
